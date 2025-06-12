123ef38b23f23bbfeeed9190071595d65de3fb2c
Up240134 <up240134@alumnos.upa.edu.mx>
2025-06-11 18:50:53 -0600

    Dia1

---
+++ day01level02.py
@@ -0,0 +1,54 @@
+# Suma
+print("suma")
+print(3+4)
+
+# Resta
+print("resta")
+print(3 - 4)
+
+# Multiplicacion
+print("Multiplicacion")
+print(3 * 4)
+
+# Módulo (resta después de la división)
+print("Módulo")
+print(3 % 4)
+
+# Division
+print("Division")
+print(3 / 4)
+
+# Exponencial (potencia)
+print("Exponencial")
+print(3 ** 4)
+
+# División de piso (cociente redondeado hacia abajo)
+print("División de piso")
+print(3 // 4)
+
+# Prompt user for personal data
+name = input("Ingresa tu nombre")
+family_name = input("Introduzca su apellido:")
+country = input("Introduzca su país:")
+
+# Print the data using print()
+print("\n--- Your Submitted Data ---")
+print(name)
+print(family_name)
+print(country)
+print("I am enjoying 30 days of python")
+
+
+# Numeric types
+type(10)            # Integer (int)
+type(9.8)           # Float
+type(3.14)          # Float
+type(4 - 4j)        # Complex number
+
+# List
+type(["José"'Python', 'México'])  # List
+
+# Strings (replace placeholders with your data)
+type("Your name")        # String (str)
+type("Your family name") # String (str)
+type("Your country")     # String (str)
