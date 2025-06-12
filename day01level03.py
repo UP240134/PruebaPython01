8a339148d8834cce90b2c91cc5a26f1d873011fb
Up240134 <up240134@alumnos.upa.edu.mx>
2025-06-11 18:59:31 -0600

    Dia 1 Completo

---
+++ day01level03.py
@@ -0,0 +1,35 @@
+# Integer (Number)
+age = 30
+print(type(age))  # Output: <class 'int'>
+
+# Float (Number)
+height = 1.75
+print(type(height))  # Output: <class 'float'>
+
+# Complex (Number)
+complex_num = 3 + 4j
+print(type(complex_num))  # Output: <class 'complex'>
+
+# String
+name = "Alice"
+print(type(name))  # Output: <class 'str'>
+
+# Boolean
+is_python_fun = True
+print(type(is_python_fun))  # Output: <class 'bool'>
+
+# List (Mutable sequence)
+languages = ["Python", "JavaScript", "Java"]
+print(type(languages))  # Output: <class 'list'>
+
+# Tuple (Immutable sequence)
+coordinates = (40.7128, -74.0060)
+print(type(coordinates))  # Output: <class 'tuple'>
+
+# Set (Unordered unique elements)
+unique_numbers = {1, 2, 2, 3, 4}
+print(type(unique_numbers))  # Output: <class 'set'> (Value: {1, 2, 3, 4})
+
+# Dictionary (Key-value pairs)
+person = {"name": "Bob", "age": 25, "country": "Canada"}
+print(type(person))  # Output: <class 'dict'>
